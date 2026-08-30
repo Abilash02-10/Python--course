80f89a92ce624f6c8e955d5df09ec0c1a94eec3e
Abilash02-10 <abilashreddyalugonda@gmail.com>
2026-08-30 08:15:26 -0700

    Data types can define kind of value a vairable can hold

---
+++ Day3/datatypes.py
@@ -0,0 +1,38 @@
+# Data Types
+
+# Integer
+x = 10
+age = 12
+product_quality = 5
+order_id = 100045
+
+# String
+name = "rohit"
+
+# Float
+price = 749.33
+discount = 15.5
+rating = 4.3
+
+# Complex
+z = 5 + 2j
+
+# Sequence Data Types
+
+# String
+name = "rohit sharma"
+
+# List
+cart_items = ["shoes", "watch", "bag"]
+
+# Print values
+print("x =", x)
+print("name =", name)
+print("price =", price)
+print("age =", age)
+print("product_quality =", product_quality)
+print("order_id =", order_id)
+print("discount =", discount)
+print("rating =", rating)
+print("complex number =", z)
+print("cart items =", cart_items)
