fa8ecdb626affc96a85464b06f3f96bdcbccf98e
Abilash02-10 <abilashreddyalugonda@gmail.com>
2026-09-01 09:02:11 -0700

    datetime built in libraies

---
+++ Day26/fileoperations.py
@@ -0,0 +1,29 @@
+'''file = open('pfs-63.txt','r')
+print(file.read())
+file.seek(0)
+print(file.readline())
+file.seek(0)
+print(file.readlines())
+file.close()'''
+
+'''with open ('pfs-63.txt','r') as file:
+    print(file.read())
+    file.seek(0)
+    print(file.readline())
+    file.seek(0)
+    print(file.readlines())'''
+
+'''with open ('pfs-633.txt','w') as file:
+    file.write("shifted to branch -1")'''
+
+'''with open ('pfs-633.txt','a') as file:
+
+    file.write("close the file")
+'''
+
+'''with open('pfs-633.txt','a+') as file:
+    file.write("Tom same branch 5")
+    file.seek(0)
+    print(file.read())'''
+
+
