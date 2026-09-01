ac126b01b8c609e800f98e8ea25396efe62d5a7d
Abilash02-10 <abilashreddyalugonda@gmail.com>
2026-09-01 08:31:18 -0700

    keywors and patterns

---
+++ Day15/keywords.py
@@ -0,0 +1,10 @@
+'''if 10 % 2==0:
+    pass'''
+
+'''for i in range(10):
+    pass'''
+'''
+def function():
+    pass'''
+
+
---
+++ Day15/patterns.py
@@ -0,0 +1,46 @@
+'''for i in range(5):
+    print('*', end=" ")
+    '''
+
+'''for i in range(20):
+    for j in range(10):
+        print("*", end=" ")
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(i, end = "")
+    print()
+'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(j, end = " ")
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(i + j, end = " ")
+    print()'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(j %2, end = " ")
+    print()
+'''
+
+'''for i in range(5):
+    for j in range(5):
+        print(i %2, end = " ")
+    print() '''
+
+'''for i in range(5):
+    for j in range(5):
+        print((i + j)%2, end = " ")
+    print()
+    '''
+'''for i in range(5):
+    for j in range(5 - i):
+        print('*', end = " ")
+    print()
+'''
