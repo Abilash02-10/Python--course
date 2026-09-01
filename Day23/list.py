e692621f9d2a2d7ecf465523b71c2d99074670b4
Abilash02-10 <abilashreddyalugonda@gmail.com>
2026-09-01 08:57:42 -0700

    list mutable data set in python

---
+++ Day23/list.py
@@ -0,0 +1,21 @@
+'''n = int(input())
+res = [i for i in range(1, n + 1)]
+print(res)'''
+
+'''num = list(map(int,input().split()))
+res = [ x for x in num if x % 2 == 0]
+print(res)'''
+
+'''words = input().split
+res = [ word.upper() for word in words ]
+print(res)'''
+
+'''words = input().split()
+res = [len(word) for word in words]
+print(res)'''
+
+'''num = list(map(int,input().split()))
+res = [x for x in num if x % 3 == 0 and x % 5 == 0]
+print(res)'''
+
+
